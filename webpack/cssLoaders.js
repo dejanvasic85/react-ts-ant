97c6d7e44b2723daa154e5f02076c57922ee6c2a
Dejan Vasic <dejanvasic24@gmail.com>
2020-04-16 08:04:45 +1000

    Minor refactor for style references

---
+++ webpack/cssLoaders.js
@@ -0,0 +1,45 @@
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+
+const { localIdentName, rootPath } = require('./env');
+const themeFile = fs.readFileSync(`${rootPath}/src/styles/theme.less`, 'utf-8');
+const modifyVars = lessToJs(themeFile, { resolveVariables: true, stripPrefix: true });
+
+const postCssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: [autoprefixer()],
+  },
+};
+
+// Ant Design does not use CSS modules and pass the theme variables
+module.exports.antStyleLoaders = [
+  {
+    loader: 'css-loader',
+  },
+  postCssLoader,
+  {
+    loader: 'less-loader',
+    options: {
+      javascriptEnabled: true,
+      modifyVars,
+    },
+  },
+];
+
+// Here, we do USE css modules
+module.exports.cssStyleLoaders = [
+  {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        localIdentName,
+      },
+    },
+  },
+  postCssLoader,
+  {
+    loader: 'less-loader',
+  },
+];
--- webpack/loaders.js
+++ webpack/loaders.js
@@ -1,53 +1,8 @@
-const fs = require('fs');
-const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const lessToJs = require('less-vars-to-js');
 
-const { isDevEnvironment, excludedFolders, localIdentName, imageBundleLocation, rootPath } = require('./env');
-
-const themeFile = fs.readFileSync(`${rootPath}/src/styles/theme.less`, 'utf-8');
-const modifyVars = lessToJs(themeFile, { resolveVariables: true, stripPrefix: true });
-
+const { isDevEnvironment, excludedFolders, imageBundleLocation } = require('./env');
+const { antStyleLoaders, cssStyleLoaders } = require('./cssLoaders');
 const getFullHashedUrl = (file) => `${file}/[name].[hash:8].[ext]`;
-
-const antCssLoaders = [
-  {
-    loader: 'css-loader',
-  },
-  {
-    loader: 'postcss-loader',
-    options: {
-      plugins: [autoprefixer()],
-    },
-  },
-  {
-    loader: 'less-loader',
-    options: {
-      javascriptEnabled: true,
-      modifyVars,
-    },
-  },
-];
-
-const cssLoaders = [
-  {
-    loader: 'css-loader',
-    options: {
-      modules: {
-        localIdentName,
-      },
-    },
-  },
-  {
-    loader: 'postcss-loader',
-    options: {
-      plugins: [autoprefixer()],
-    },
-  },
-  {
-    loader: 'less-loader',
-  },
-];
 
 module.exports = [
   {
@@ -57,12 +12,12 @@ module.exports = [
   },
   {
     test: /antd.*\.less$/,
-    loader: isDevEnvironment ? ['style-loader', ...antCssLoaders] : [MiniCssExtractPlugin.loader, ...antCssLoaders],
+    loader: isDevEnvironment ? ['style-loader', ...antStyleLoaders] : [MiniCssExtractPlugin.loader, ...antStyleLoaders],
   },
   {
     test: /\.less?$/,
     exclude: excludedFolders,
-    loader: isDevEnvironment ? ['style-loader', ...cssLoaders] : [MiniCssExtractPlugin.loader, ...cssLoaders],
+    loader: isDevEnvironment ? ['style-loader', ...cssStyleLoaders] : [MiniCssExtractPlugin.loader, ...cssStyleLoaders],
   },
   {
     test: /\.svg?$/,
