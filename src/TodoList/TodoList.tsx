84ed45f6fc4ef464f9c537e269cb14fe6ccbcf1e
Dejan Vasic <dejanvasic24@gmail.com>
2020-07-10 21:20:03 +1000

    Playing around with some animations

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Button, Result } from 'antd';
-import { SmileOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Button, Result, Input } from 'antd';
+import { SmileOutlined, StopOutlined } from '@ant-design/icons';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+import styles from './TodoList.less';
 
 const TodoList = () => {
+  const [showInput, setShowInput] = useState(false);
+
+  console.log('styles', styles);
   return (
-    <Result
-      icon={<SmileOutlined />}
-      title="Great, we have done all the operations!"
-      extra={<Button type="primary">Add Task</Button>}
-    />
+    <div>
+      <Result
+        icon={<SmileOutlined />}
+        title="Great, we have done all the operations!"
+        extra={
+          <Button type="primary" onClick={() => setShowInput(true)}>
+            Add Task
+          </Button>
+        }
+      />
+
+      <CSSTransition
+        unmountOnExit={true}
+        in={showInput}
+        classNames={{
+          enter: styles.inputEnter,
+          enterActive: styles.inputEnterActive,
+          exit: styles.inputExit,
+          exitActive: styles.inputExitActive,
+        }}
+        timeout={1000}
+      >
+        <div>
+          <Input placeholder="Take rubbish out..." />
+          <Button onClick={() => setShowInput(false)}>
+            <StopOutlined />
+          </Button>
+        </div>
+      </CSSTransition>
+    </div>
   );
 };
 
